ea40680f77a24aef971c69a48299e060ab44ef79
André da Silva <afsilvas@ua.pt>
2016-12-28 16:45:22 +0000

    Change Password function completed and storyboard views corrected

--- Photoshare/ChangePWViewController.swift
+++ Photoshare/ChangePWViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ChangePWViewController: UIViewController {
 
+    @IBOutlet var email: UITextField!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,19 +25,66 @@ class ChangePWViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func ChangePWB(_ sender: Any) {
+    @IBAction func RequestPW(_ sender: Any) {
+        if self.email.text == "" || self.email.text != FIRAuth.auth()?.currentUser?.email {
+            let alertController = UIAlertController(title: "Error!", message: "Please enter your E-Mail!!", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            FIRAuth.auth()?.sendPasswordReset(withEmail: self.email.text!, completion: { (error) in
+                
+                var title = ""
+                var message = ""
+                
+                if error != nil
+                {
+                    title = "Oops!"
+                    message = (error?.localizedDescription)!
+                }
+                else
+                {
+                    title = "Success!"
+                    message = "Password reset email sent."
+                    self.email.text = ""
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            })
+        }
+        
+        
+        /* old stuff
+         if let user = FIRAuth.auth()?.currentUser {
+         print("::[DEBUGGER]:: Authenticated check")
+         //change to new password
+         user.updatePassword(new_pw.text!, completion: { (error) in
+         if let error = error {
+         print("::[DEBUGGER]:: God knows why this failed")
+         print(error.localizedDescription)
+         } else {
+         print("::[DEGUBBER]:: UPDATED successfully")
+         
+         let alert2 = UIAlertController(title: "Password Change", message:
+         "You have successfully changed your Password", preferredStyle: UIAlertControllerStyle.alert)
+         alert2.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+         
+         self.present(alert2, animated: true, completion: nil)
+         }
+         })
+         }*/
+        // function end
+
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
